71ce9941c66c6a5e80b0efdbc1901a58f9ca37f9
Gustavo Fonseca <gustavoh.m.fonseca@gmail.com>
2021-06-19 17:07:38 -0300

    Primeiro commit sobre a seção de fundamento de python, ocorrido na aula 241, sobre comentários tendo explicações, sobre o que são comentários e como utilizá-los, e exemplos sobre o assunto.

---
+++ README.md
@@ -0,0 +1,2 @@
+Arquivos do módulo de fundamentos de python, do curso de Desenvolvimento web full stack com python e django.
+obs:Recomendo utilizar o pycharm para melhor visualização e execução dos arquivos
---
+++ main.py
@@ -0,0 +1,14 @@
+#comentario
+"""
+comentario 
+comentario
+"""
+salario = 1000
+desconto = 200
+"""
+O desconto é multiplicado por 2 em
+razão das faltas contabilizaddas, tendo mais 
+de 3 faltas o desconto é dobrado.
+"""
+total = salario - (2*desconto)
+print(total)#comentario
