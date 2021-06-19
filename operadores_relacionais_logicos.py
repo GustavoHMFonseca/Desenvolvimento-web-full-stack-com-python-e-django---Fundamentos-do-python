22681c30445a221494b786477f5d181b7e54c62d
Gustavo Fonseca <gustavoh.m.fonseca@gmail.com>
2021-06-19 19:15:11 -0300

    Commit sobre a aula 244 sobre operadore lógicos e operadores relacionais explicando o que são e demonstrando exemplos.

---
+++ operadores_relacionais_logicos.py
@@ -0,0 +1,32 @@
+"""
+Operadores relacionais(retorna verdadeiro ou falso)
+== (igual a)
+!= (diferente)
+>  (maior que)
+<  (menor que)
+>= (maior ou igual)
+<= (menor ou igual)
+"""
+
+# print(1 == 1)
+# print(1 != 2)
+# print(3 > 2)
+# print(2 < 3)
+# print(2 >= 2)
+# print(3 =< 3)
+
+"""
+Operadores lógicos(testa condições com true ou false)
+and(e)
+or(ou)
+"""
+
+# idade >50 ou totalCompra > 200 20%
+idade = 15
+totalCompra = 200
+print( idade >= 50 or totalCompra >= 200)
+
+
+
+
+
