9d80f804a2d56aac44a002dde815c35078c927ff
Gustavo Fonseca <gustavoh.m.fonseca@gmail.com>
2021-06-19 20:28:11 -0300

    Commit sobre a aula 247 sobre operações com textos. Essa foi a primeira parte e ensinou sobre selecionar caracteres de um teto, exibir parte do texto, como mostrar dentro de uma string aspas simples e dupla sobre texto direto onde mostra o texto da maneira exata em que foi escrito.

---
+++ operacoes_textos.py
@@ -0,0 +1,23 @@
+"""
+Operações com textos
+"""
+
+texto = "carro"
+# print(texto[4])
+# print(texto[-2])
+# print(texto[2:5])
+# print(texto[::-1])
+
+# frase = 'Meu nome é \'Gustavo\''
+#frase = 'Meu nome é ' \
+#        'Gustavo'
+# frase = 'Meu nome é \n Gustavo'
+frase = """
+    123456
+    Teste
+    ....
+Teste
+"""
+
+print(frase)
+
