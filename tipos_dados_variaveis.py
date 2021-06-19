497e747b5797484ad80ec61fbbcd42265818cec6
Gustavo Fonseca <gustavoh.m.fonseca@gmail.com>
2021-06-19 18:19:27 -0300

    Commit sobre o material da aula 242 sobre tipos de dados, variáveis e constantes. Apresentando os tipos diferentes de dados e como utilizá-los, variáveis e como utilizá-las e constantes, que não são implementadas em python mas por convenção utiliza variáveis escritas em maiúsculo para indicar uma constante, mesmo ainda sendo uma variável.

---
+++ tipos_dados_variaveis.py
@@ -0,0 +1,20 @@
+"""
+print(True)#booleans
+print(False)
+print(1.5 + 3.5)#float
+print("Gustavo")#string
+print('também string')
+print(["Teste1", "Teste2","Teste3"])#lista
+
+idade = 32
+idade = "Texto"
+print(idade)
+
+"""
+
+altura = 1.73
+peso = 72
+
+imc = peso / (altura * altura)
+PI = 3.14
+print(imc)
