590e28a3281929a348fc25734f5c134db8d7d992
Gustavo Fonseca <gustavoh.m.fonseca@gmail.com>
2021-06-19 18:41:07 -0300

    Commit sobre a aula 243 sobre operadores aritméticos e unários onde é explicado o que são e demonstrados diversos exemplos de somas, subtrações, etc. Após isso é explicado sobre precedência de operadores.

---
+++ operadores-aritmeticos.py
@@ -0,0 +1,39 @@
+"""
+Operadores aritméticos(binários)
+somar(+)
+subtrair(-)
+multiplicar(*)
+dividir(/)
+exponenciação(**)
+módulo(%) ->resto da divisão
+"""
+
+numero1 = 10
+numero2 = 2
+
+resultado = numero1 % numero2
+
+# print(resultado)
+# Operadores Unários
+"""
+    valor +=1
+    valor -=1
+    valor /=1
+    valor *=1
+    valor **=2
+"""
+valor = 10
+
+print(valor)
+
+"""
+Precedência de operadores
+0) Parênteses
+1) Multiplicação e Divisão
+2) Adição e Subtração
+"""
+# resultado = 2 + 2 * 2
+# resultado = 2 + 2 / 2 * 3
+resultado = (2 - 10) * 3
+print(resultado)
+
