fe56367940a86c24bcedd9fecb09cda448d03e60
Gustavo Fonseca <gustavoh.m.fonseca@gmail.com>
2021-06-19 20:08:04 -0300

    Commit sobra a ula 246 sobre builtins, funções que ja vem instaladas no python, e conversão de tipos, tanto manuais quanto automáticas como seus usos.

---
+++ buitins.py
@@ -0,0 +1,21 @@
+# builtins
+# print("Gustavo")
+# print(type("Gustavo"))
+# print(dir())
+__builtins__.print("Gustavo")
+
+"""
+Conversão de tipos
+Tipos: str, int ,float, list, bool
+"""
+# print(1 + int('2'))
+# print(1 + float('2.5'))
+# numero = 9
+# print(str(numero) + '50')
+
+"""
+Conversão automática
+"""
+# print(type(10/2))
+print(type(2 + True))
+
