5c1fd627bfbcf84f61fe1ac6679b7f80b279b3a8
Gustavo Fonseca <gustavoh.m.fonseca@gmail.com>
2021-06-20 00:53:43 -0300

    Commit sobre a aula 252 sobre concatenação de textos.A aula mostra diversos metodos e exemplos de concatenação entre textos e números.

---
+++ interpolacao_texto.py
@@ -0,0 +1,11 @@
+carro = 'Fusca'
+preco = 1200
+
+# print('carro: ' + carro + ', preco: ' + str(preco))
+
+# versão mais antigas
+# print('carro : %s, preco: %.2f' % (carro, preco))
+# print('carro: {0}, preco: {1}' .format(carro, preco))
+
+# versão mais nova
+print(f'carro: {carro}, preco: {preco}')
