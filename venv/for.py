c43ccea201866df157693b803c4b284cee3bf437
Gustavo Fonseca <gustavoh.m.fonseca@gmail.com>
2021-06-20 02:37:43 -0300

    Commit sobre a aula 254 sobre for. É explicado o funcionamento e exemplificado com outras formas de tipos de dados como textos, tuplas e sets.

---
+++ venv/for.py
@@ -0,0 +1,49 @@
+# postagens = [
+#     "Hoje passeando pela avenida paulista",
+#     "Fazendo trilha na pedra do Gavião",
+#     "hoje fiz um  curso de criação de sistemas",
+#     "Na casa da mãe, almoçando todos juntos"
+# ]
+# totalPostagen = len(postagens)
+# contador = 0
+# while contador <=len(postagens) - 1:
+#     print(f'{contador + 1} - {postagens[contador]}')
+#     contador += 1
+#     print("+++++++++++++++++")
+
+postagens = [
+    "Hoje passeando pela avenida paulista",
+    "Fazendo trilha na pedra do Gavião",
+    "hoje fiz um  curso de criação de sistemas",
+    "Na casa da mãe, almoçando todos juntos"
+]
+# for postagem in postagens:
+#     print(postagem)
+#     print("+++++++++++++++++")
+
+# nome, nome2 = "Gustavo", "Fonseca"
+# print(nome)
+# print(nome2)
+
+# for indice, postagem in enumerate(postagens):
+#     print(f'{indice + 1} - {postagem}')
+#     print("+++++++++++++++++")
+
+# totalPostagens = len(postagens)
+# for indice in range(totalPostagens):#(0:11)
+#     print(postagens[indice])
+
+"""
+Percorrendo textos, tuplas e sets
+"""
+# palavra = "paralelepipedo"
+# for letra in palavra:
+#     print(f' - {letra} -')
+
+# meses = ('Janeiro','Fevereiro', 'Março')
+# for mes in meses:
+#     print(f' - {mes} -')
+
+frutas={'banana','maça','abacaxi','melancia'}
+for frutas in frutas:
+    print(f' - {frutas} -')
