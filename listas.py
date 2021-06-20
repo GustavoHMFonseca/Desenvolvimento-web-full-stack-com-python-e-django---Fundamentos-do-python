cae9a7c683acbb3141dbe7092278655189db5c30
Gustavo Fonseca <gustavoh.m.fonseca@gmail.com>
2021-06-19 21:31:57 -0300

    Commit sobra a aula 249 sobre listas.Foi ensinado e exemplificado diversas funções das listas como contar a quantidade de itens verificar se algum item esta incluso na lista, etc.

---
+++ listas.py
@@ -0,0 +1,24 @@
+"""
+Listas são como assays em Javascript
+Mutável e dinâmica, heterogênea e indexada
+"""
+
+lista = ["Pedro","joão","maria", "ana", 10, 1.5]
+# print(type(lista))
+# print(dir(lista))
+# print(lista[0:2])
+# print(lista[::-1])
+
+# lista[0] = "alterado"
+# lista.append("Silvia")
+# lista.remove("Pedro")
+# del lista[0:2]
+
+# print(len(lista))
+# print(lista.count("Pedro"))
+# print(lista.index('ana'))
+# lista.sort()
+print("Pedro" in lista)
+print("Juca" not in lista)
+print(lista)
+
