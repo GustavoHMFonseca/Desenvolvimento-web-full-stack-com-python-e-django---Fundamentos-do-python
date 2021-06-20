9c3eafcec516762d802db58ecd4ba29d806a307c
Gustavo Fonseca <gustavoh.m.fonseca@gmail.com>
2021-06-20 12:32:14 -0300

    Commit sobre a aula 257 sobre funçoes de primeira classe e de primeira ordem. Funções de primeira classe é demonstrar que em python funções pode ser utilizadas como tipos de dados embutidos na liguagem sendo armazenadas em variáveis e em listas. Funções de primeira ordem são funções que recebem e/ou retornam outras funções.

---
+++ venv/funcoes_primeira_classe_ordem.py
@@ -0,0 +1,41 @@
+"""
+Funções como objetos de primeira classe, são funções que se
+comportam como qualquer tipo nativo de uma determinada linguagem.
+"""
+
+def somar(a, b):
+    return a + b
+
+
+def subtrair(a, b):
+    return a - b
+
+# lista = [somar, subtrair]
+# for funcao in lista:
+#     resultado = funcao(5, 6)
+#     print(f'Resultado: {resultado}')
+
+# a = somar
+# print(a(1,2))
+
+"""
+Funções de ordem superior são funções que recebem funções como
+parâmetro(s) e/ou retornam funções como resposta
+"""
+
+carrinho_compras = [
+    {'produto':'Fone de Ouvido', 'preco' : 500},
+    {'produto':'Controle Xbox', 'preco' : 400},
+    {'produto':'Celular', 'preco' : 800},
+]
+
+print(carrinho_compras)
+
+def calcular_desconto(lista, funcao):
+    for produto in lista:
+        #print(produto['preco'])
+        item_desconto = funcao(produto['preco'], 10)
+        print(item_desconto)
+
+
+calcular_desconto(carrinho_compras,somar)
