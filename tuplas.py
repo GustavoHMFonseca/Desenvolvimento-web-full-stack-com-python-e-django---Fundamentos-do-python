f544b5fff9b4974a083e58865d14a717c361989b
Gustavo Fonseca <gustavoh.m.fonseca@gmail.com>
2021-06-19 21:51:04 -0300

    Commit sobre a aula 250 sobre tuplas.Tuplas são básicamente listas mas que não pode alterar incluir ou remover nenhum novo elemento após sua criação.

---
+++ tuplas.py
@@ -0,0 +1,21 @@
+"""
+Tuplas
+"""
+# tupla = tuple()
+# lista = list()
+
+# tupla = (10) não é tupla é apenas um inteiro
+# tupla = (10,) é uma tupla
+# print(type(tupla))
+
+tupla= ("Pedro" , "João", "Ana", "Maria", "Joana", 13)
+# print(tupla[0])
+# print(tupla)
+# print(tupla[-1])
+# print(tupla[0:2])
+# print(tupla[::-1])
+# tupla[0] ="alterado"
+# print(len(tupla))
+# print(tupla.index("Pedro"))
+# print(tupla.count("Joana"))
+print(tupla)
